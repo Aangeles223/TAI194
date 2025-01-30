5508a00f61642aad6202a252ea43dd61057b3507
Aangeles223 <122044096@upq.edu.mx>
2025-01-29 18:08:48 -0600

    Practica 2

--- FAST/main.py
+++ FAST/main.py
@@ -2,6 +2,20 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def main():
-    return{"message": "!Bienvenido a FasAPI!"}
+    return{"HELLO": "Hello World"}
+
+
+#EndPoint promedio
+@app.get("/promedio")
+def promedio():
+    return 10.5
+
+
+#EndPoint con parametro obligatorio
+@app.get("/usuario/{id}")
+def consultausuario(id:int):
+    #caso ficticia de busqueda en BD
+    return {"Se encontro el usuario":id}
